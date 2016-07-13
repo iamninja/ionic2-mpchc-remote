397b45c6d55c952e4c23b59a37750834c59c82ce
Vagios Vlachos <Vagios Vlachos>
2016-07-14 01:10:45 +0300

    Add basic controls

--- app/pages/controls/controls.ts
+++ app/pages/controls/controls.ts
@@ -1,10 +1,20 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 
+import { MpchcService } from '../../services/mpchc.service';
+
 @Component({
   templateUrl: 'build/pages/controls/controls.html'
 })
 export class ControlsPage {
-  constructor(private navController: NavController) {
+  constructor(
+      private navController: NavController,
+      private mpchcService: MpchcService) {
+  }
+
+  basicCommand(command: string) {
+      this.mpchcService.basicCommand(command)
+        .then(() => console.log("Good"))
+        .catch((err) => console.log(err));
   }
 }
